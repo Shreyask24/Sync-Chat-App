26aa52398ea72674af122acf45868e5855456ffe
Shreyask24 <shreyask495@gmail.com>
2024-10-18 20:15:02 +0530

    Added feature to close chat and display the Username on the header

--- client/src/pages/chat/components/chat-container/new-dm/index.jsx
+++ client/src/pages/chat/components/chat-container/new-dm/index.jsx
@@ -78,35 +78,38 @@ const NewDM = () => {
                         <Input onChange={(e) => searchContacts(e.target.value)} className="rounded-lg p-6 bg-[#2c2e3b] border-none" placeholder="Search Contacts" />
                     </div>
 
-                    <ScrollArea className="h-[250px]">
-                        <div className="flex flex-col gap-5">
-                            {
-                                searchedContacts.map((contact) => (
-                                    <div key={contact._id} className="flex gap-3 items-center cursor-pointer" onClick={() => selectNewContact(contact)}>
-                                        <div className="w-12 h-12 relative">
-                                            <Avatar className='h-12 w-12 rounded-full overflow-hidden'>
-                                                {contact.image ? (<AvatarImage src={`${HOST}/${contact.image}`} alt="profile" className="object-cover w-full h-full bg-black" />) : (<div className={` uppercase h-12 w-12 text-lg border-[1px] flex justify-center items-center rounded-full ${getColor(contact.color)}`} >
-                                                    {contact.firstName ? contact.firstName.split("").shift() : contact.email.split("").shift()}
-                                                </div>)}
-                                            </Avatar>
-                                        </div>
-                                        <div className="flex flex-col">
-                                            <span>
+                    {searchedContacts.length > 0 && (
+                        <ScrollArea className="h-[250px]">
+                            <div className="flex flex-col gap-5">
+                                {
+                                    searchedContacts.map((contact) => (
+                                        <div key={contact._id} className="flex gap-3 items-center cursor-pointer" onClick={() => selectNewContact(contact)}>
+                                            <div className="w-12 h-12 relative">
+                                                <Avatar className='h-12 w-12 rounded-full overflow-hidden'>
+                                                    {contact.image ? (<AvatarImage src={`${HOST}/${contact.image}`} alt="profile" className="object-cover w-full h-full bg-black" />) : (<div className={` uppercase h-12 w-12 text-lg border-[1px] flex justify-center items-center rounded-full ${getColor(contact.color)}`} >
+                                                        {contact.firstName ? contact.firstName.split("").shift() : contact.email.split("").shift()}
+                                                    </div>)}
+                                                </Avatar>
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <span>
 
-                                                {
-                                                    contact.firstName && contact.lastName ? `${contact.firstName} ${contact.lastName}` : contact.email
-                                                }
-                                            </span>
-                                            <span className="text-xs">{contact.email}</span>
+                                                    {
+                                                        contact.firstName && contact.lastName ? `${contact.firstName} ${contact.lastName}` : contact.email
+                                                    }
+                                                </span>
+                                                <span className="text-xs">{contact.email}</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </ScrollArea>
+                                    ))
+                                }
+                            </div>
+                        </ScrollArea>
+                    )}
+
                     {
                         searchedContacts.length <= 0 && (
-                            <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col mt-5 justify-center items-center duration-1000 transition-all">
+                            <div className="flex-1 md:flex flex-col mt-5 md:mt-0 justify-center items-center duration-1000 transition-all">
                                 <Lottie isClickToPauseDisabled={true} height={100} width={100} options={animationDefaultOptions} />
 
                                 <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-5 lg:text-2xl text-xl transition-all duration-300 text-center">
