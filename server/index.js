cb016fc06c528a74f579fd0d34b7da692c887e00
Shreyask24 <shreyask495@gmail.com>
2024-10-22 15:58:08 +0530

    Done with the Socket setup and also created new model for messages

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import authRoutes from "./routes/AuthRoute.js"
 import contactRoutes from "./routes/ContactRoutes.js"
+import setupSocket from "./socket.js"
 
 dotenv.config();
 
@@ -32,5 +32,7 @@ app.use("/api/contacts", contactRoutes)
 const server = app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}/`)
 })
+
+setupSocket(server)
 
 mongoose.connect(databaseURL).then(() => console.log("Database Connected Successfully.")).catch((err) => console.log(err.message))
