3eec8dc6d3d9a0f913b126831fd3e54481dc009f
Shreyask24 <shreyask495@gmail.com>
2025-08-28 00:42:30 +0530

    redirect issue solved from backend, cookie safety

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,132 +1,195 @@
-import Victory from "@/assets/victory.svg"
-import Background from "@/assets/login2.png"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { useState } from "react"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { toast } from "sonner"
-import { apiClient } from "@/lib/api-client"
-import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants"
-import { useNavigate } from "react-router-dom"
-import { useAppStore } from "@/store"
-
+import Victory from "@/assets/victory.svg";
+import Background from "@/assets/login2.png";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { apiClient } from "@/lib/api-client";
+import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants";
+import { useNavigate } from "react-router-dom";
+import { useAppStore } from "@/store";
 
 const Auth = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const { setUserInfo } = useAppStore()
+  const { setUserInfo } = useAppStore();
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setconfirmPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
 
-    function validateSignup() {
-        if (!email.length) {
-            toast.error("Email is required")
-            return false
-        }
-        if (!password.length) {
-            toast.error("Password is required")
-            return false
-        }
-        if (password !== confirmPassword) {
-            toast.error("Password and confirm password should be same.")
-            return false
-        }
-        return true
+  function validateSignup() {
+    if (!email.length) {
+      toast.error("Email is required");
+      return false;
     }
-
-    function validateLogin() {
-        if (!email.length) {
-            toast.error("Email is required")
-            return false
-        }
-        if (!password.length) {
-            toast.error("Password is required")
-            return false
-        }
-        return true
+    if (!password.length) {
+      toast.error("Password is required");
+      return false;
     }
-
-
-    const handleLogin = async () => {
-        try {
-            if (validateLogin()) {
-                const response = await apiClient.post(LOGIN_ROUTE, { email, password }, { withCredentials: true })
-                if (response.data.user.id) {
-                    setUserInfo(response.data.user)
-                    if (response.data.user.profileSetup) navigate("/chat")
-                    else navigate("/profile")
-                }
-                toast.message("User Login Successfull")
-            }
-        } catch (error) {
-            const errMsg = error?.response?.data || "Something went wrong";
-            toast.error(errMsg);
-
-        }
+    if (password !== confirmPassword) {
+      toast.error("Password and confirm password should be same.");
+      return false;
     }
+    return true;
+  }
 
-    const handleSignup = async () => {
-        try {
-            if (validateSignup()) {
-                const response = await apiClient.post(SIGNUP_ROUTE, { email, password }, { withCredentials: true })
-                if (response.status === 201) {
-                    setUserInfo(response.data.user)
-                    navigate("/profile")
-                }
-                toast.message("User Created Successfully")
-            }
-        } catch (error) {
-            const errMsg = error?.response?.data || "Something went wrong"
-            toast.error(errMsg)
-        }
-
-
+  function validateLogin() {
+    if (!email.length) {
+      toast.error("Email is required");
+      return false;
     }
-    return (
-        <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-            <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
-                <div className="flex flex-col gap-10 items-center justify-center">
+    if (!password.length) {
+      toast.error("Password is required");
+      return false;
+    }
+    return true;
+  }
 
-                    <div className="flex flex-col items-center justify-center gap-7">
-                        <div className="flex justify-center items-center">
-                            <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
-                            <img src={Victory} alt="Victory Emoji" className="h-[100px]" />
-                        </div>
-                        <p className="font-medium text-center">Fill in the details to get started with the best chat app!</p>
-                    </div>
+  const handleLogin = async () => {
+    try {
+      if (validateLogin()) {
+        const response = await apiClient.post(
+          LOGIN_ROUTE,
+          { email, password },
+          { withCredentials: true }
+        );
+        if (response.data.user.id) {
+          setUserInfo(response.data.user);
+          if (response.data.user.profileSetup) navigate("/chat");
+          else navigate("/profile");
+        }
+        toast.message("User Login Successfull");
+      }
+    } catch (error) {
+      const errMsg = error?.response?.data || "Something went wrong";
+      toast.error(errMsg);
+    }
+  };
 
-                    <div className="flex items-center justify-center w-full">
-                        <Tabs defaultValue="login" className="w-3/4">
-                            <TabsList className="bg-transparent rounded-none w-full">
-                                <TabsTrigger value="login" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Login</TabsTrigger>
-
-                                <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">SignUp</TabsTrigger>
-                            </TabsList>
-                            <TabsContent value="login" className="flex flex-col gap-5 mt-10">
-                                <Input placeholder="Email" type="email" onChange={(e) => setEmail(e.target.value)} value={email} className="rounded-full p-6" />
-                                <Input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} value={password} className="rounded-full p-6" />
-                                <Button className="rounded-full p-6" onClick={handleLogin}>Login</Button>
-                            </TabsContent>
-
-                            <TabsContent value="signup" className="flex flex-col gap-5 mt-1">
-                                <Input placeholder="Email" type="email" onChange={(e) => setEmail(e.target.value)} value={email} className="rounded-full p-6" />
-                                <Input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} value={password} className="rounded-full p-6" />
-                                <Input placeholder="Confirm Password" type="password" onChange={(e) => setconfirmPassword(e.target.value)} value={confirmPassword} className="rounded-full p-6" />
-                                <Button className="rounded-full p-6" onClick={handleSignup}>Sign Up</Button>
-                            </TabsContent>
-                        </Tabs>
-
-                    </div>
-                </div>
-
-                <div className="hidden xl:flex justify-center items-center">
-                    <img src={Background} alt="background login" className="h-[500px]" />
-                </div>
+  const handleSignup = async () => {
+    try {
+      if (validateSignup()) {
+        const response = await apiClient.post(
+          SIGNUP_ROUTE,
+          { email, password },
+          { withCredentials: true }
+        );
+        if (response.status === 201) {
+          setUserInfo(response.data.user);
+          navigate("/profile");
+        }
+        toast.message("User Created Successfully");
+      }
+    } catch (error) {
+      const errMsg = error?.response?.data || "Something went wrong";
+      toast.error(errMsg);
+    }
+  };
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex flex-col items-center justify-center gap-7">
+            <div className="flex justify-center items-center">
+              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+              <img src={Victory} alt="Victory Emoji" className="h-[100px]" />
             </div>
-        </div>
-    )
-}
+            <p className="font-medium text-center">
+              Fill in the details to get started with the best chat app!
+            </p>
+          </div>
 
-export default Auth
+          <div className="flex items-center justify-center w-full">
+            <Tabs defaultValue="login" className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  Login
+                </TabsTrigger>
+
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  SignUp
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="login" className="flex flex-col gap-5 mt-10">
+                <form
+                  className="flex flex-col gap-5"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleLogin();
+                  }}
+                >
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    className="rounded-full p-6"
+                  />
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    className="rounded-full p-6"
+                  />
+                  <Button className="rounded-full p-6" onClick={handleLogin}>
+                    Login
+                  </Button>
+                </form>
+              </TabsContent>
+
+              <TabsContent value="signup" className="flex flex-col gap-5 mt-1">
+                <form
+                  className="flex flex-col gap-5"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSignup();
+                  }}
+                >
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    className="rounded-full p-6"
+                  />
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    className="rounded-full p-6"
+                  />
+                  <Input
+                    placeholder="Confirm Password"
+                    type="password"
+                    onChange={(e) => setconfirmPassword(e.target.value)}
+                    value={confirmPassword}
+                    className="rounded-full p-6"
+                  />
+                  <Button className="rounded-full p-6" onClick={handleSignup}>
+                    Sign Up
+                  </Button>
+                </form>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+
+        <div className="hidden xl:flex justify-center items-center">
+          <img src={Background} alt="background login" className="h-[500px]" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,204 +1,208 @@
-import { compare } from "bcrypt"
-import User from "../models/UserModel.js"
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
-import { renameSync, unlinkSync } from "fs"
+import { compare } from "bcrypt";
+import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { renameSync, unlinkSync } from "fs";
 
 dotenv.config();
 
-const expireTime = 3 * 24 * 6 * 60 * 1000
+const expireTime = 3 * 24 * 60 * 60 * 1000; // 3 days
 
 const createToken = (email, userId) => {
-    return jwt.sign({ email, userId }, process.env.JWT_KEY, { expiresIn: expireTime })
-}
+  return jwt.sign({ email, userId }, process.env.JWT_KEY, {
+    expiresIn: expireTime / 1000,
+  });
+  // expiresIn expects seconds, not ms
+};
 
 export const signup = async (req, res, next) => {
-    try {
-        const { email, password } = req.body
+  try {
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(400).send("Email & Password is required")
-        }
-
-        const user = await User.create({
-            email, password
-        })
-
-        res.cookie("jwt", createToken(email, user.id), {
-            expireTime,
-            secure: true,
-            sameSite: "None"
-        });
-
-        return res.status(201).json({
-            user: {
-                id: user.id,
-                email: user.email,
-                profileSetup: user.profileSetup,
-            }
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("Internal Server Error")
+    if (!email || !password) {
+      return res.status(400).send("Email & Password is required");
     }
-}
+
+    const user = await User.create({
+      email,
+      password,
+    });
+
+    res.cookie("jwt", createToken(email, user.id), {
+      maxAge: expireTime, // correct way
+      httpOnly: true, // important for security
+      secure: true, // only send over HTTPS
+      sameSite: "None", // required for cross-site cookies (e.g., frontend + backend on different domains)
+    });
+
+    return res.status(201).json({
+      user: {
+        id: user.id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+      },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export const login = async (req, res, next) => {
-    try {
-        const { email, password } = req.body
+  try {
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(400).send("Email & Password is required")
-        }
-
-        const user = await User.findOne({ email })
-
-        if (!user) {
-            return res.status(404).send("User with the given email not found")
-        }
-
-        const auth = await compare(password, user.password)
-
-        if (!auth) {
-            return res.status(404).send("Password is incorrect")
-        }
-
-        res.cookie("jwt", createToken(email, user.id), {
-            expireTime,
-            secure: true,
-            sameSite: "None"
-        });
-
-        return res.status(200).json({
-            user: {
-                id: user.id,
-                email: user.email,
-                profileSetup: user.profileSetup,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                image: user.image,
-                color: user.color,
-            }
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("Internal Server Error")
+    if (!email || !password) {
+      return res.status(400).send("Email & Password is required");
     }
-}
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).send("User with the given email not found");
+    }
+
+    const auth = await compare(password, user.password);
+
+    if (!auth) {
+      return res.status(404).send("Password is incorrect");
+    }
+
+    res.cookie("jwt", createToken(email, user.id), {
+      maxAge: expireTime, // correct way
+      httpOnly: true, // important for security
+      secure: true, // only send over HTTPS
+      sameSite: "None", // required for cross-site cookies (e.g., frontend + backend on different domains)
+    });
+
+    return res.status(200).json({
+      user: {
+        id: user.id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        image: user.image,
+        color: user.color,
+      },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export const getUserInfo = async (req, res, next) => {
-    try {
-        const userData = User.findById(req.userId)
+  try {
+    const userData = await User.findById(req.userId); // <-- await!
 
-        if (!userData) {
-            res.status(404).send("User with the given ID not found")
-        }
-
-        return res.status(200).json({
-            id: userData.id,
-            email: userData.email,
-            profileSetup: userData.profileSetup,
-            firstName: userData.firstName,
-            lastName: userData.lastName,
-            image: userData.image,
-            color: userData.color,
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("Internal Server Error")
+    if (!userData) {
+      return res.status(404).send("User with the given ID not found"); // <-- return
     }
 
-}
-
+    return res.status(200).json({
+      id: userData._id, // Mongoose uses _id
+      email: userData.email,
+      profileSetup: userData.profileSetup,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export const updateProfile = async (req, res, next) => {
-    try {
-        const { userId } = req
-        const { firstName, lastName, color } = req.body
+  try {
+    const { userId } = req;
+    const { firstName, lastName, color } = req.body;
 
-        if (!firstName || !lastName) return res.status(400).send("Firstname lastname color is required.")
+    if (!firstName || !lastName)
+      return res.status(400).send("Firstname lastname color is required.");
 
-        const userData = await User.findByIdAndUpdate(userId, { firstName, lastName, color, profileSetup: true }, { new: true, runValidators: true })
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      { firstName, lastName, color, profileSetup: true },
+      { new: true, runValidators: true }
+    );
 
-        return res.status(200).json({
-            id: userData.id,
-            email: userData.email,
-            profileSetup: userData.profileSetup,
-            firstName: userData.firstName,
-            lastName: userData.lastName,
-            image: userData.image,
-            color: userData.color,
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("Internal Server Error")
-    }
-
-}
-
+    return res.status(200).json({
+      id: userData.id,
+      email: userData.email,
+      profileSetup: userData.profileSetup,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export const addProfileImage = async (req, res, next) => {
-    try {
-
-        if (!req.file) {
-            return res.status(400).send("File is required")
-        }
-
-        const date = Date.now()
-        let fileName = "uploads/profiles/" + date + req.file.originalname;
-        renameSync(req.file.path, fileName)
-
-        const updatedUser = await User.findByIdAndUpdate(req.userId, { image: fileName }, { new: true, runValidators: true })
-
-        return res.status(200).json({
-            image: updatedUser.image,
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("Internal Server Error")
+  try {
+    if (!req.file) {
+      return res.status(400).send("File is required");
     }
 
-}
+    const date = Date.now();
+    let fileName = "uploads/profiles/" + date + req.file.originalname;
+    renameSync(req.file.path, fileName);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.userId,
+      { image: fileName },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      image: updatedUser.image,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export const removeProfileImage = async (req, res, next) => {
-    try {
-        const { userId } = req
-        const user = await User.findById(userId)
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
 
-        if (!user) {
-            return res.status(404).send("User not found")
-        }
-
-        if (user.image) {
-            unlinkSync(user.image)
-        }
-
-        user.image = null;
-        await user.save();
-
-        return res.status(200).send("Profile Image removed successfully")
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("Internal Server Error")
+    if (!user) {
+      return res.status(404).send("User not found");
     }
 
-}
+    if (user.image) {
+      unlinkSync(user.image);
+    }
+
+    user.image = null;
+    await user.save();
+
+    return res.status(200).send("Profile Image removed successfully");
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export const logout = async (req, res, next) => {
-    try {
-
-        res.cookie("jwt", "", { expireTime: 1, secure: true, sameSite: "None" })
-        return res.status(200).send("Logout successfull")
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("Internal Server Error")
-    }
-
-}
+  try {
+    res.cookie("jwt", "", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 0, // Immediately expire
+    });
+    return res.status(200).send("Logout successful");
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
